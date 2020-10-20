5784e36b1ff61137e8788e95656d8f9a55aaeab4
hcc96923 <hcc96923@github.com>
2020-10-20 17:29:29 +0800

    优化

--- 框架/simple-react/index.js
+++ 框架/simple-react/index.js
@@ -34,6 +34,10 @@ class Conuter extends React.Component {
         // react会将多个setState的调用合并成一个来执行，这意味着setState的时候state并不会立即执行
         
     }
+    // 类组件的render方法以及函数式组件的返回值均为element
+    // 虚拟dom是对组件实例或者dom节点的描述
+    // 每次组件props或者state变更首先会反映到虚拟dom树，然后最终反应到页面dom节点树的渲染
+    // 而且这个纯对象包含两个属性：type:(string|ReactClass)和props:Object
     render() { 
         // transform-react-jsx是将jsx转换为js的babel插件
         // 它有一个pragma项，可以定义jsx转换方法的名称。
